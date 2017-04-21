6b4be0f0c1a4fb6611ccf4c6aff2706793254566
Jorge Hernández Ramírez <jorgehernandezramirez@MacBook-Pro-de-Jorge.local>
2017-04-21 18:24:29 +0200

    no message

--- src/main/java/com/jorgehernandezramirez/spring/springboot/rabbitmq/config/RabbitConfiguration.java
+++ src/main/java/com/jorgehernandezramirez/spring/springboot/rabbitmq/config/RabbitConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,14 +18,29 @@ public class RabbitConfiguration {
 
     private static final String EXCHANGE_NAME = "test-queue-exchange";
 
+    @Value("${rabbitmq.host}")
+    private String host;
+
+    @Value("${rabbitmq.port}")
+    private Integer port;
+
+    @Value("${rabbitmq.user}")
+    private String user;
+
+    @Value("${rabbitmq.password}")
+    private String password;
+
+    @Value("${rabbitmq.virtualhost}")
+    private String virtualhost;
+
     @Bean
     public ConnectionFactory connectionFactory() throws Exception {
         final CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setAddresses("192.168.99.100");
-        connectionFactory.setPort(5672);
-        connectionFactory.setUsername("admin");
-        connectionFactory.setPassword("admin");
-        connectionFactory.setVirtualHost("/");
+        connectionFactory.setAddresses(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(user);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualhost);
         return connectionFactory;
     }
 
